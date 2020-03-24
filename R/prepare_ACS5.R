baf36b906738e6ed16b6c0b3545176e91d582817
jacksonmluckey <jacksonmluckey@gmail.com>
2020-03-24 14:58:53 -0700

    this part will pull down acs5 data once it is complete I will merge with prepare_df and then stick into index.Rmd with some if statements to only have it run if the resulting RDAs are not in the Data/ folder

---
+++ R/prepare_ACS5.R
@@ -0,0 +1,70 @@
+# load the necessary packages
+library(tidyverse)
+library(tidycensus)
+library(stringr)
+
+####################
+# census functions #
+####################
+make_census_table_wide <- function(df) {
+  
+  df <- df %>%
+    select(-label, -concept) %>%
+    pivot_longer(cols = c(estimate, moe)) %>%
+    pivot_wider(names_from = c(name, variable), values_from = value)
+  
+}
+
+# removes " County, Ohio" from county column (because it's the same in all cases)
+clean_county_col<- function(df) {
+  
+  df <- df %>%
+    mutate(county = str_remove(county, " County, Ohio"))
+  
+}
+
+clean_tidycensus_table <- function(df) {
+  
+  # rename the "NAME" column to county
+  df <- df %>%
+    rename(county = NAME)
+  
+  df <- clean_county_col(df)
+  
+}
+
+get_census_table <- function(table, year) {
+  df <- get_acs("county",
+                year = year,
+                table = table,
+                survey = "acs1", # if things break for older code make a version of this function with this line removed
+                state = "OH")
+  df <- clean_tidycensus_table(df)
+}
+
+# wrapper for get_census_table that handles a year range
+get_census_table_multiple_years <- function(table, years) {
+  count <- 0
+  for (year in years) {
+    tmp <- get_census_table(table, year)
+    tmp$year <- year
+    if (count == 0) {
+      df <- tmp
+      count <- 1
+    } else {
+      df <- bind_rows(tmp, df)
+    }
+  }
+  df
+}
+
+var_labels <- function(table) {
+  load_variables(2010, "acs1", cache = TRUE) %>%
+    filter(str_detect(name, paste0(table, "_"))) %>%
+    rename(variable = name)
+}
+
+########################
+# NEW CODE BEGINS HERE #
+########################
+
