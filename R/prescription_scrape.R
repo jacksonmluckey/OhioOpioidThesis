27aeba3c6ba2f16621c4f1446815d90160c0211e
jacksonmluckey <jacksonmluckey@gmail.com>
2020-04-03 13:03:33 -0700

    almost done; rvest is stupid easy to use

--- R/prescription_scrape.R
+++ R/prescription_scrape.R
@@ -1,2 +1,22 @@
 library(tidyverse)
 library(rvest)
+library(glue)
+
+# creates the URL to grab for a particular year
+url_year <- function(year) {
+  year <- as.character(year)
+  url <- glue("https://www.cdc.gov/drugoverdose/maps/rxcounty{year}.html")
+  url
+}
+
+prescriptionYear <- function(year) {
+  url_year(year) %>%
+    read_html() %>%
+    html_node("table") %>%
+    html_table() %>%
+    filter(State == "OH") %>%
+    mutate(year = year)
+}
+
+# years covered in CDC data
+years <- 2006:2018
