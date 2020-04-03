b24d42e528766d0ff0438ecedf3f27d6fae43fc7
jacksonmluckey <jacksonmluckey@gmail.com>
2020-04-03 13:11:35 -0700

    working on fixing the duplicated prescribing rate column issue

--- R/prescription_scrape.R
+++ R/prescription_scrape.R
@@ -4,19 +4,26 @@ library(glue)
 
 # creates the URL to grab for a particular year
 url_year <- function(year) {
-  year <- as.character(year)
   url <- glue("https://www.cdc.gov/drugoverdose/maps/rxcounty{year}.html")
   url
 }
 
 prescriptionYear <- function(year) {
-  url_year(year) %>%
+  year <- as.character(year)
+  df <- url_year(year) %>%
     read_html() %>%
     html_node("table") %>%
     html_table() %>%
-    filter(State == "OH") %>%
     mutate(year = year)
+  col_to_rename <- glue("{Year} Prescribing Rate")
+  df <- df %>%
+    filter(State == "OH") %>%
+    rename()
 }
+
+test <- prescriptionYear(2017)
 
 # years covered in CDC data
 years <- 2006:2018
+
+prescriptions <- map_dfr(years, prescriptionYear)
