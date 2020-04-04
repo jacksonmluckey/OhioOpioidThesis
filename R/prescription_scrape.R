5bbc3d1877ef927e37f12708617346c639377b8f
jacksonmluckey <jacksonmluckey@gmail.com>
2020-04-03 19:38:34 -0700

    finished; ready to move into index.Rmd

--- R/prescription_scrape.R
+++ R/prescription_scrape.R
@@ -2,28 +2,24 @@ library(tidyverse)
 library(rvest)
 library(glue)
 
-# creates the URL to grab for a particular year
-url_year <- function(year) {
-  url <- glue("https://www.cdc.gov/drugoverdose/maps/rxcounty{year}.html")
-  url
-}
-
+# download prescription data for one particular year
 prescriptionYear <- function(year) {
   year <- as.character(year)
-  df <- url_year(year) %>%
+  df <- glue("https://www.cdc.gov/drugoverdose/maps/rxcounty{year}.html") %>%
     read_html() %>%
     html_node("table") %>%
     html_table() %>%
     mutate(year = year)
-  col_to_rename <- glue("{Year} Prescribing Rate")
+  colnames(df) <- c("county", "State", "FIPS", "PrescribingRate", "year")
   df <- df %>%
     filter(State == "OH") %>%
-    rename()
+    select(-State, -FIPS) %>%
+    mutate(county = str_remove(county, ", OH"))
 }
-
-test <- prescriptionYear(2017)
 
 # years covered in CDC data
 years <- 2006:2018
 
-prescriptions <- map_dfr(years, prescriptionYear)
+prescriptions <- map_dfr(years, prescriptionYear) %>%
+  mutate(county = as_factor(county),
+         PrescribingRate = as.numeric(PrescribingRate))
