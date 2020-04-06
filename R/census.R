b047fe9d1e0a1315454898fae2f91a5aae506a22
jacksonmluckey <jacksonmluckey@gmail.com>
2020-04-06 14:40:44 -0700

    deleted code I won't need

--- R/census.R
+++ R/census.R
@@ -4,28 +4,6 @@ library(glue)
 
 apis <- listCensusApis()
 
-get_url <- function(year, glue_string, valid_years) {
-  year <- as.character(year)
-  valid_years <- as_vector(as.character(valid_years))
-  if(year %in% valid_years) {
-    glue(glue_string)
-  } else {
-    warning(glue("Please input a year between {valid_years[1]} and {valid_years[length(valid_years)]}. {year} is out of range"))
-  }
-}
-
-get_url_acs1_2005_to_2009 <- function(year) {
-    get_url(year = year,
-            glue_string = glue("https://api.census.gov/data/{year}/acs/acs1"),
-            valid_years = 2005:2009)
-}
-
-get_url_acs1_2010_to_2018 <- function(year) {
-  get_url(year = year,
-          glue_string = glue("https://api.census.gov/data/{year}/acs/acs1"),
-          valid_years = 2010:2018)
-}
-
 # identify useful variables
 acs1_vars <- listCensusMetadata("acs/acs1", vintage = 2018, type = "variables")
 acs1_vars_income <- acs1_vars %>%
@@ -46,16 +24,19 @@ income_concepts <- enframe(unique(acs1_vars_income$concept)) %>%
 # HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN {year} INFLATION-ADJUSTED DOLLARS)
 # group: B19001
 
-get_census_year <- function(api_name, year, group) {
+get_census_year <- function(api_name, year, vars) {
   census_year <- getCensus("acs/acs1",
                            vintage = year,
+                           vars = vars,
                            region = "county:*",
-                           regionin = "state:39":
-                           group = "B19001")
+                           regionin = "state:39")
+  census_year
 }
+
 get_census_multiple_years <- function(api_name, years, group) {
   
 }
+
 for(year in 2005:2018) {
   print(year)
   if(year == 2005) {
@@ -75,3 +56,5 @@ for(year in 2005:2018) {
             bind_rows(test)
   }
 }
+
+# B19013_001E = median household income in last 12 months in that year's inflation adjusted dollars
